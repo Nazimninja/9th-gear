e6163bac6ee90354dcad47bba6edcb528c7b4ee2
9th Gear AI <nazim@9thgear.co.in>
2026-02-18 12:38:04 +0530

    chore: Cloud-Ready Update (Sheets JSON + Human Delays)

--- index.js
+++ index.js
@@ -62,10 +62,6 @@ const client = new Client({
             '--disable-gpu',
             '--disable-features=site-per-process'
         ]
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
 });
 
